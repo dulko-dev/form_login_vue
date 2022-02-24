8df7a3590bd5e63a944cab2b240611f9b3256e36
Kamil Duliniec <123dulko@wp.pl>
2022-02-24 21:48:27 +0100

    add more unit tests to Login Component

--- src/__test__/Login.test.js
+++ src/__test__/Login.test.js
@@ -5,6 +5,7 @@ import mystore from "../store/index.js";
 import { routes } from "../router/index.js";
 import { createRouter, createWebHistory } from "vue-router";
 import { createStore } from "vuex";
+import User from "../views/User.vue";
 
 const store = createStore(mystore);
 const router = createRouter({
@@ -31,6 +32,31 @@ describe("Login option", () => {
     await fireEvent.update(inputEmail, "admin@dev.pl");
     expect(inputEmail.value).toBe("admin@dev.pl");
   });
+  test("find input password", async () => {
+    const inputPassword = screen.getByPlaceholderText(/password/i);
+    await fireEvent.update(inputPassword, "password");
+    expect(inputPassword.value).toBe("password");
+  });
 });
 
-
+describe("log in to user", () => {
+  beforeEach(() => {
+    render(Login, wrapper);
+  });
+  test("click on guestLogin", async () => {
+    const button = screen.getByRole("button", { name: "Login as Guest" });
+    await fireEvent.click(button).then(() => render(User, wrapper));
+    const logOutBtn = screen.getByRole("button", { name: "LogOut" });
+    expect(logOutBtn).toBeInTheDocument();
+  });
+  test("click on login", async () => {
+    const inputLogin = screen.getByPlaceholderText(/email/i);
+    const password = screen.getByPlaceholderText(/password/i);
+    const btnLogin = screen.getByRole("button", { name: "Login" });
+    await fireEvent.update(inputLogin, "admin@dev.pl");
+    await fireEvent.update(password, "123test");
+    await fireEvent.click(btnLogin).then(() => render(User, wrapper));
+    const logOutBtn = screen.getByRole("button", { name: "LogOut" });
+    expect(logOutBtn).toBeInTheDocument();
+  });
+});
