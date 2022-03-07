81a7c5932fc568a5b9130d5e00bb8f84de663b05
Kamil Duliniec <123dulko@wp.pl>
2022-03-07 20:35:51 +0100

    structure of calendar

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="calendar">Calendar</div>
+  <div class="calendar">
+    <div class="calendar__container">
+      <div class="calendar__container--data">
+        <div class="calendar__container--data--month">April</div>
+        <div class="calendar__container--data--year">
+          <pre>&lt;</pre>
+          <p>2022</p>
+          <pre>&gt;</pre>
+        </div>
+      </div>
+      <div class="calendar__container--main">
+        <div class="calendar__container--main--days">
+          <div>Sun</div>
+          <div>Mon</div>
+          <div>Tue</div>
+          <div>Wed</div>
+          <div>Thu</div>
+          <div>Fri</div>
+          <div>Sat</div>
+        </div>
+        <div class="calendar__container--main--daysOfMonth">
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
